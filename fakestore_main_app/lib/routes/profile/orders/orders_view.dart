6f3ca99e9cf4e59491e4cb9b16746a5ef0dd926c
Tri <acumen.us@gmail.com>
2023-01-19 07:34:52 +0700

    #13 Update Orders view

--- fakestore_main_app/lib/routes/profile/orders/orders_view.dart
+++ fakestore_main_app/lib/routes/profile/orders/orders_view.dart
@@ -1,7 +1,11 @@
 import 'package:fakestore_main_app/routes/profile/orders/orders_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../../app_utils.dart';
+import '../../../ui/app_ios_navigation_bar.dart';
 
 class ProfileOrders extends StatelessWidget {
   ProfileOrders(
@@ -13,15 +17,43 @@ class ProfileOrders extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: ElevatedButton(
-          child: Text(controller.currentSelectedTab.value.name),
-          onPressed: () {
+    return CupertinoPageScaffold(
+      navigationBar: FSIOSNavigationBar.create(
+          middleText: AppUtils.getLocalizationContext(context).orders,
+          backButtonPressed: () {
             context.pop();
-          },
-        ),
+          }),
+      child: Stack(
+        children: [],
       ),
     );
+  }
+
+  List<Widget> _buildMainStackViewChildren() {
+    List<Widget> list = [];
+    list.add(Container(
+      color: Colors.white,
+      child: Container(),
+    ));
+    if (controller.isLoading.isTrue) {
+      list.add(Container(
+        child: Stack(
+          children: [
+            // black background
+            Container(
+              color: Colors.black.withOpacity(0.3),
+            ),
+            // circular progress indicator
+            Center(
+              child: const CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ));
+    }
+
+    return list;
   }
 }
